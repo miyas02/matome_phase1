d9e91ea41416106f090f89c5e7ed6c1e88d29251
miyas <aabc.aabc.zx@gmail.com>
2026-05-06 04:44:46 +0900

    【ScraperConfigValidator】テストコード実装。E1実装完了。追加

--- matome_phase1.Tests/ScraperConfigValidatorTest.cs
+++ matome_phase1.Tests/ScraperConfigValidatorTest.cs
@@ -1,36 +1,52 @@
-﻿using System;
+﻿using matome_phase1.constants;
+using matome_phase1.Core.scraper;
+using matome_phase1.scraper.Configs;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using matome_phase1.Core.scraper;
 
 namespace matome_phase1.Tests.ScraperConfigValidatorUnitTest {
     public class ScraperConfigValidatorTest {
-        public static IEnumerable<object[]> TestCases => new[] {
-            new object[] { "5ch", @"TestFiles/5ch_ScraperConfig.json", @"TestFiles/5ch_DocParseItems_Expect.json", @"log/5ch_DocParseItems_Actual.json", @"TestFiles/targetHtml.html" }
+        static string projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", ".."));
+        static string E1 = @$"{projectRoot}/TestFiles/ScraperConfigValidationTestFiles/E1_ValidationExtract_ChildMissingRequiredSet.json";
+        public static TheoryData<string, ScraperConfigValidationErrorCode> TestCases => new (){
+            {E1, ScraperConfigValidationErrorCode.InvalidFormat}
         };
-        [Fact]
-        public void ValidateTest() {
+        
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void ValidateTest(string inputJson, ScraperConfigValidationErrorCode expectedErrorCode) {
             //Arrange
-            string json = """
-                [
-                    {
-                       "URL": "https://example.com"
-                    }
-                ]
-                """;
-            JsonDocument doc = JsonDocument.Parse(json);
-            var root = doc.RootElement;
+            //logger
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(projectRoot)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            var inputFile = File.ReadAllText(inputJson);
+            var inputDocument = JsonDocument.Parse(inputFile);
             try {
                 //Act
-                ScraperConfigValidator.Validate(doc);
-
-            } catch (Exception ex) {
+                Log.Information($"Validate実行：{inputJson}");
+                ScraperConfigValidator.Validate(inputDocument);
+            } catch (ScraperConfigValidationException ex) {
                 //Assert
-                Assert.True(true, $"Validation failed with exception: {ex.Message}");
-            }   
+                Log.Information(
+                    "ScraperConfig validation result. Expected={Expected}, Actual={Actual}",
+                    expectedErrorCode,
+                    ex.ErrorCode);
+                Assert.Equal(ex.ErrorCode, expectedErrorCode);
+            }
+            Log.CloseAndFlush();
         }
     }
 }
